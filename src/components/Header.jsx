dd68db24d6b4111436dee8c86bac58dc56211b86
fadeyibigbolahan <fadeyibi26@gmail.com>
2024-09-23 11:56:14 +0100

    okaay o

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full p-5 mb-4">
       <img src={capstone} className="w-1/2 md:w-1/4" />
-      <p className="font-bold text-white my-2 text-center text-sm md:text-[25px]">
+      <p className="font-bold text-white my-2 text-center text-sm md:text-[30px]">
         Get A Guaranteed Cash Offer That Won't Change.
       </p>
-      <p className="text-white my-2 md:w-3/5 w-4/5 text-center text-xs md:text-[20px] leading-6">
+      <p className="text-white my-2 w-full text-center text-xs md:text-[20px] leading-6">
         Sell Your House Fast For Cash. Zero Closing Costs, No Fees. You Choose
         the Closing Date. Sell As-Is Without Repairs or Cleaning. Take What You
         Want and Leave the Rest!
